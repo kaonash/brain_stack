51e7a5ea62352e0b6c45d0c24378d64a5722da3a
ken.shimizu <ken.shimizu@uzabase.com>
2016-04-24 18:23:20 +0900

    ignore prod.secret.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,15 @@ use Mix.Config
 config :brain_stack, BrainStack.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :brain_stack, BrainStack.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  url: System.get_env("DATABASE_URL"),
+  database: "brain_stack",
+  port: 3316,
+  pool_size: 10
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +70,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"

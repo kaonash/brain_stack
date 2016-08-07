e245851f5fbf05f0b329447787fef4deb9295728
ken.shimizu <ken.shimizu@uzabase.com>
2016-08-08 00:29:15 +0900

    slack対応

--- web/controllers/item_controller.ex
+++ web/controllers/item_controller.ex
@@ -4,7 +4,14 @@ defmodule BrainStack.ItemController do
   alias BrainStack.ElasticSearch.Query
   alias BrainStack.Item
 
-  def new(conn, %{"title" => title, "text" => text} = params) do
+  # def new(conn, %{"title" => title, "text" => text} = params) do
+    # item = Item.new(title, text)
+    # {:ok, 200, _} = Query.insert(item)
+    # render(conn, "top.html")
+  # end
+
+  def new(conn, %{"text" => text} = params) do
+    title = "test"
     item = Item.new(title, text)
     {:ok, 200, _} = Query.insert(item)
     render(conn, "top.html")
